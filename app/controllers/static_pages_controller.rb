a6da9c2888935fd57a9eb8b813e7a9743d011724
James Myers <yesmantoozu2@hotmail.com>
2015-11-03 16:24:02 -0800

    fixing routing errors

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+  def contact
+    #redirect_to static_pages.contact
+  end
+
+  def portraits
+  end
+
+  def who_I_am
+  end
+
+  def blog
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  #resources :homes
-  resources :users, only: [:new, :create]
+  get 'static_pages/contact'
 
+  get 'static_pages/portraits'
+
+  get 'static_pages/who_I_am'
+
+  get 'static_pages/blog'
+
+  root 'homes#index'
+  resources :homes
+  resources :users, only: [:new, :create]
+  resources :static_page
   # Create a better looking URL for logging in
   resources :sessions, only: [:new, :create, :destroy]
   get '/login', to: 'sessions#new'
