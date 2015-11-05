8ac1f248b83a8213fb600950f223d1f28aafa71e
James Myers <yesmantoozu2@hotmail.com>
2015-11-05 14:07:53 -0800

    fixed editing since did not work

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -19,8 +19,8 @@ class PhotosController < ApplicationController
   end
   def update
     @photo = Photo.find(params[:id])
-    if @photo.update(params[:description, :url])
-      redirect_to gallery_path(params[:gallery_id])
+    if @photo.update(photo_params)
+      redirect_to gallery_path(@photo.gallery_id)
     else
       render :action => 'edit'
     end
